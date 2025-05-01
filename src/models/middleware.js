c68137dd91600925659ac542c3d0731d866eedc6
user.name "Aritra Bag <user.email "aritrabag5427@gmail.com>
2025-05-01 20:32:24 +0530

    update middleware

--- src/models/middleware.js
+++ src/models/middleware.js
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req){
-    const token=await getToken({req, secret: process.env.NEXTAUTH_SECRET});
+    const token=await getToken({req, secret: process.env.NEXT_PUBLIC_CALLBACK_URL});
     //if user try to go /user-auth page after login
     if(req.nextUrl.pathname ==='/user-auth' && token){
         return NextResponse.redirect(new URL('/', req.url));
