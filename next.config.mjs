6f40820ecfe94f868fec5c54ee48a5f4f69af62e
user.name "Aritra Bag <user.email "aritrabag5427@gmail.com>
2025-05-01 21:20:39 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          has: [
+            {
+              type: 'cookie',
+              key: 'next-auth.session-token', // For HTTP
+              value: '^(?!.*)' // No token
+            },
+            {
+              type: 'cookie',
+              key: '__Secure-next-auth.session-token', // For HTTPS (used by Vercel, etc.)
+              value: '^(?!.*)'
+            }
+          ],
+          destination: '/user-auth',
+          permanent: false,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
